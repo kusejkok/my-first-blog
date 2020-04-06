891ad5d049b05c124f7a7919d4968377b0c6d914
Kathi <katharina.kusejko@hotmail.com>
2020-04-06 13:52:15 +0200

    Cancel and drafts of posts

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('about/', views.about_page, name='about_page'),
     path('terminal/', views.terminal_description, name='terminal_description'),
 ]
